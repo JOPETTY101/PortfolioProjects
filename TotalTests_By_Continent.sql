54e87be8f6b0655b2525f2b7f5ee61ec5cb54692
JOPETTY101 <jo.petty.13@gmail.com>
2021-07-31 13:04:39 -0500

    Delete TotalTests_By_Continent.sql

--- TotalTests_By_Continent.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [continent]
-      ,[TotalPopulation]
-      ,[TotalCases]
-      ,[TotalTests]
-      ,[TestsPerPopulation]
-  FROM [TestDB].[dbo].[TotalTests_By_Continent]
-  ORDER BY TestsPerPopulation DESC
